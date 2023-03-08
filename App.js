25ff5e1b325f91268c3c75afa61a7bebdf8b0b72
Chesnokov <91004755+B1ad@users.noreply.github.com>
2023-03-08 07:41:43 +0000

    feat: 6

--- App.js
+++ App.js
@@ -7,6 +7,10 @@ import Start from './pages/Start'
 import IntroFirst from './pages/Intro1';
 import IntroSecond from './pages/Intro2';
 import IntroThird from './pages/Intro3';
+import SignUp from './pages/SignUp';
+import Dictionary from './pages/Dict';
+import Training from './pages/Training'
+import Video from './pages/Video'
 
 const Stack = createStackNavigator();
 
@@ -32,6 +36,26 @@ export default function App() {
         <Stack.Screen
           name="Intro-3"
           component={IntroThird}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Dictionary"
+          component={Dictionary}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Training"
+          component={Training}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Video"
+          component={Video}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
