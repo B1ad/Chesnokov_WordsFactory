729c3eb290db5b7a45296d74dfc02fbcb77befa5
Chesnokov <91004755+B1ad@users.noreply.github.com>
2023-03-08 07:39:58 +0000

    feat: 4

--- App.js
+++ App.js
@@ -1,43 +1,16 @@
-import AppLoading from 'expo-app-loading';
-import React, { useCallback, useEffect } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Start from './pages/Start'
-// import { useFontsHook } from './hooks/useFonts';
 import IntroFirst from './pages/Intro1';
 import IntroSecond from './pages/Intro2';
 import IntroThird from './pages/Intro3';
-import SignUp from './pages/SignUp';
-import Dictionary from './pages/Dict';
-import Training from './pages/Training'
-import Video from './pages/Video'
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  // const [fontsLoaded] = useFonts({
-  //   'rubik-bold': Rubik_700Bold,
-  //   'rubik-medium': Rubik_500Medium,
-  //   'rubik-regular': Rubik_400Regular,
-  // })
-  // const [fontsLoaded] = useFonts({
-  //   'rubik-bold': require('./assets/fonts/Rubik-Bold.ttf'),
-  //   'rubik-medium': require('./assets/fonts/Rubik-Medium.ttf'),
-  //   'rubik-regular': require('./assets/fonts/Rubik-Regular.ttf'),
-  // });
-
-  // if (!fontsLoaded) { 
-  //   console.log('пошел нахуй')
-  //   return <AppLoading />
-  // }
-
-  // const LoadFonts = async () => {
-  //   await useFontsHook();
-  // };
-
   return (
       <NavigationContainer>
       <Stack.Navigator>
@@ -61,40 +34,7 @@ export default function App() {
           component={IntroThird}
           options={{ headerShown: false }}
         />
-        <Stack.Screen
-          name="SignUp"
-          component={SignUp}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Dictionary"
-          component={Dictionary}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Video"
-          component={Video}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Training"
-          component={Training}
-          options={{ headerShown: false }}
-        />
       </Stack.Navigator>
     </NavigationContainer> 
   )
-
-  // return (
-  //   <Start/>
-  // );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fdf',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
